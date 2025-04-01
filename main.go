9b85d9c80e86a5e1101fbf19e3d77da9a898ec78
davilapa <diegoavilapava@gmail.com>
2025-04-01 16:37:21 -0500

    removing unused import

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
